12df95eddb952b6dbe4b520a4c80f4534bcab7ae
Tamtam(Asahi Tamura) <130454351+atamu2463@users.noreply.github.com>
2026-06-27 16:45:24 +0900

    Fix: backendコンテナでPrismaClientを確実に生成できるようにする (#60)
    * Fix: Db接続のためDriver Adaperを導入
    
    * Fix: nest起動時に環境変数をロードさせるため、ConfigModuleを導入
    
    * Fix: Nest起動と環境変数読み込みのズレがあるため、インスタンス生成時にアダプターをsuperに渡す形に変更

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const url = process.env.DATABASE_URL!;
+    if (!url) {
+      throw new Error("DATABASE_URLが定義されていません");
+    }
+    const pool = new PrismaPg({ connectionString: url });
+    super({ adapter: pool });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
