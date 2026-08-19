c73a09602d8c12f4126344421c143d0766ac5346
Tamtam(Asahi Tamura) <130454351+atamu2463@users.noreply.github.com>
2026-08-19 22:14:42 +0900

    feat: PostgreSQLへのDB接続処理を実装  (#86)
    * feat: GORM, Postgresドライバを導入
    
    * feat: DB接続処理を実装

---
+++ backend/config/database.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
