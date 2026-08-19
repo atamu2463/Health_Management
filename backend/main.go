f769883c4e658ede87530a1d1043a684026fa097
atamu263 <asahiwork.cording@gmail.com>
2026-08-19 20:21:34 +0900

    feat: DB接続処理を実装

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"backend/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := config.ConnectDB()
+	if err != nil {
+		log.Fatal("DB接続に失敗しました", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("DBインスタンスの取得に失敗しました", err)
+	}
+
+	fmt.Println("DB接続に成功しました")
+
+	defer sqlDB.Close()
 }
